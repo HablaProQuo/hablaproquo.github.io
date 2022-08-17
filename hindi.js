7161c2be7648923d4ce23dde3bf7363462d4d888
Angel Alonso <96095646+HablaProQuo@users.noreply.github.com>
2022-08-17 05:52:19 +0100

    Update

---
+++ hindi.js
@@ -0,0 +1,613 @@
+    var velocidad = 0.8, speed = 0.9;
+    var paquete = "com.google.android.tts";
+
+v1="हां"
+v2="नहीं"
+v3 = "सुप्रभात"
+v4 = "शुभ दोपहर"
+v5 = "सहायता"
+v6="मैं नहाने जा रहा हूँ"
+v7="क्या आपके पास चार्जर है?"
+v8="आपका नाम क्या है?"
+v9="यह कितने का है?"
+v10="कहाँ?"
+v11="बाथरूम कहाँ है?"
+v12="मैं सोने जा रहा हूँ"
+v13="मुझे भूख लगी है"
+v14="कृपया"
+v15="क्या?"
+v16="आप"
+v17="मैं खुश हूं"
+v18="मैं गर्म हूं"
+v19="मैं क्रोधित हूं"
+v20="बारिश हो रही है"
+v21="यह बादल छाए हुए हैं"
+v22="यह हवा है"
+v23="यह धूप है"
+v24="क्या समय हो गया है?"
+v25="शुभ रात्रि"
+v26="क्या आप मुझे थोड़ा काट सकते हैं?"
+v27="मुझे बुरा लग रहा है"
+v28="मैं ठंडा हूँ"
+v29="बर्फबारी हो रही है"
+v30="क्या मैं खेल सकता हूँ?"
+v31="नमस्ते"
+v32="मेरा पेट दर्द करता है"
+v33="मेरे सिर में दर्द होता है"
+v34="मेरी पीठ में दर्द होता है"
+v35="मेरा गला दुखता है"
+v36="मेरी छाती में दर्द होता है"
+v37="मेरे घुटने में दर्द होता है"
+v39="कृपया अपनी दूरी बनाए रखें"
+
+function imagen1(){
+    var mensaje = v1
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen2(){
+    var mensaje = v2
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen3(){
+    var mensaje = v3
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen4(){
+    var mensaje = v4
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen5(){
+    var mensaje = v5
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen6(){
+    var mensaje = v6
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen7(){
+    var mensaje = v7
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen8(){
+    var mensaje = v8
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen9(){
+    var mensaje = v9
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen10(){
+    var mensaje = v10
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen11(){
+    var mensaje = v11
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen12(){
+    var mensaje = v12
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen13(){
+    var mensaje = v13
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen14(){
+    var mensaje = v14
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen15(){
+    var mensaje = v15
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen16(){
+    var mensaje = v16
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen17(){
+    var mensaje = v17
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen18(){
+    var mensaje = v18
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen19(){
+    var mensaje = v19
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen20(){
+    var mensaje = v20
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen21(){
+    var mensaje = v21
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen22(){
+    var mensaje = v22
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen23(){
+    var mensaje = v23
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen24(){
+    var mensaje = v24
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen25(){
+    var mensaje = v25
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen26(){
+    var mensaje = v26
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen27(){
+    var mensaje = v27
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen28(){
+    var mensaje = v28
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen29(){
+    var mensaje = v29
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen30(){
+    var mensaje = v30
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen31(){
+    var mensaje = v31
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen32(){
+    var mensaje = v32
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen33(){
+    var mensaje = v33
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen34(){
+    var mensaje = v34
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen35(){
+    var mensaje = v35
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen36(){
+    var mensaje = v36
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+function imagen37(){
+    var mensaje = v37
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
+    
+    
+    
+function imagen39(){
+    var mensaje = v39
+    if (URLdomain==="") { 
+        var velocidad = 0.8, speed = 0.9;
+        var paquete = "com.google.android.tts";
+        app.TextToSpeech( mensaje, velocidad, speed, "", "music", idioma, paquete );
+        app.ShowPopup( mensaje )
+        app.Vibrate( "0,100,30,100,50,300" );
+    } else {
+        let utterance = new SpeechSynthesisUtterance(mensaje)
+        utterance.lang = idioma
+        speechSynthesis.speak(utterance)
+    }
+}
